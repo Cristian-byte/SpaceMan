b3fbd370c91b9178adf2bd8b993db11bf49341d3
Cristian Sanchez Flores <andygign40@gmail.com>
2022-09-23 17:30:21 -0500

    1.9 Programando funciones en C# y Unity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,24 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour{
+
     //Variables del movimiento del personaje
     public float jumpForce = 6f;
-    Rigidbody2D rigidbody;
+    Rigidbody2D rigidBody;
+
+    void Awake(){
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        if (Input.GetKey(KeyCode.Space) || Input.GetMouseButtonDown(0)){
+            Jump();
+        }
     }
 
     //Salto del jugador
     void Jump(){
-
+        rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 }
