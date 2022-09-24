b21bb43bfc32ab4ad9be93a750c69ec3266f8e1a
Cristian Sanchez Flores <andygign40@gmail.com>
2022-09-23 19:23:51 -0500

    Cambiar las animaciones sugun los estados

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,16 +7,22 @@ public class PlayerController : MonoBehaviour{
     //Variables del movimiento del personaje
     public float jumpForce = 6f;
     Rigidbody2D rigidBody;
+    Animator animator;
+
+    const string STATE_ALIVE = "isAlive";
+    const string STATE_ON_THE_GROUND = "isOnTheGround";
 
     public LayerMask groundMask;
 
     void Awake(){
         rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Start is called before the first frame update
     void Start(){
-        
+        animator.SetBool(STATE_ALIVE, true);
+        animator.SetBool(STATE_ON_THE_GROUND, true);
     }
 
     // Update is called once per frame
@@ -24,6 +30,8 @@ public class PlayerController : MonoBehaviour{
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
             Jump();
         }
+
+        animator.SetBool(STATE_ON_THE_GROUND, IsTouchingTheGround());
 
         Debug.DrawRay(this.transform.position, Vector2.down * 1.3f, Color.red);
     }
@@ -40,9 +48,11 @@ public class PlayerController : MonoBehaviour{
         if(Physics2D.Raycast(this.transform.position,Vector2.down,1.5f, groundMask)){
             
             //TODO:programar logica de contacto en el suelo
+            animator.enabled = true;
             return true;
         }else{
             //TODO:programar logica de no contacto
+            animator.enabled = false;
             return false;
         }
     }
